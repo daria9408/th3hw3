5af17ed608aea516782d84b95765f99909c176b2
unknown <solnce-94.08@mail.ru>
2023-01-30 19:49:09 +0300

    exp003

--- exp003/Program.cs
+++ exp003/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int n = 5;
+
+int a = 1;
+
+while(a <= n)
+{
+    Console.WriteLine(Math.Pow(a,3));
+    a = a + 1;
+}
