03877fb4d87dd2d2ea5461bf12a4960d1b07fb69
unknown <solnce-94.08@mail.ru>
2023-01-30 19:47:48 +0300

    exp001

--- exp001/Program.cs
+++ exp001/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int a;
+a = 12821;
+
+int num1;
+num1 = a / 10000;
+
+int num2;
+num2 = a / 1000 % 10;
+
+int num3;
+num3 = a % 10;
+
+int num4;
+num4 = a / 10 % 10;
+
+if(num1 == num3)
+{
+    if(num2 == num4)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
